3d4065b75463d181be863290451cc2cc70dcb353
Cairo Campos <cairocampos98@gmail.com>
2019-03-22 09:27:12 -0300

    Documentando o projeto com comentários

--- extras/app.js
+++ extras/app.js
@@ -1,9 +1,21 @@
 function colar(){
+	//Armazenando o valor da nota informada.
 	var nota = document.getElementById("nota").value;
+
+	//Criando um novo elemento.
 	var newDiv = document.createElement("div");
+
+	//Definindo uma classe para manipular o novo elemento.
 	newDiv.setAttribute("class", "nota-area");
+
+	//Pegando o valor do nível de prioridade
 	var nivel = document.getElementById("prioridades").value;
 
+
+	/*
+	*Escopo de condição para verificar qual a escala de prioridade.
+	*Definindo cores para cada nivel.
+	*/
 	if(nivel == 1) {
 		newDiv.style.backgroundColor = '#347aba';
 	} else if (nivel == 2) {
@@ -12,21 +24,28 @@ function colar(){
 		newDiv.style.backgroundColor = 'red';
 	}
 
+	//Setando informações no corpo do novo elemento
 	newDiv.innerHTML = nota+'<a href="javascript:;" onclick="deletar(this);">X</a>';
+
+	//Atribuição do elemento ao corpo do documento.
 	var main = document.getElementsByTagName("main")[0];
 	main.appendChild(newDiv);
 
+
+	//Toda vez que uma nota for atribuida será executado essa função.
 	reset();
 }
 
+//Função que reseta o valor da nota após ser adicionada;
 function reset(){
 	var nota = document.getElementById("nota").value = "";
 
 }
 
+//Deletando a nota do documento.
 function deletar(obj) {
 	var element = obj.parentNode;
 	var parent = element.parentNode;
-	
+
 	parent.removeChild(element);
 }
